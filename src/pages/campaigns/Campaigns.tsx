00c17d8fec4e5c2549e5c17e5eaaa10f897f2d77
nishad <mdfaysalhassan@gmail.com>
2022-09-09 19:11:34 +0600

    fear: search functionility

--- src/pages/campaigns/Campaigns.tsx
+++ src/pages/campaigns/Campaigns.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import CampaignCard from "./campaignCard";
 import Category from "../../components/campaign/category";
 import { getALlCampaign } from "../../services/campaign.service";
-import classes from './campaign.module.css'
+import classes from "./campaign.module.css";
 // interface campaigns {
 //   id: string,
 //   title: string,
@@ -19,10 +19,15 @@ const Campaigns = () => {
   const [visible, setVisible] = useState(6);
   const [category, setCategory] = useState("All");
   const [error, setError] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const showMoreCampaigns = ()=>{
-    setVisible(prev=> prev + 3);
-  }
+  const showMoreCampaigns = () => {
+    setVisible((prev) => prev + 3);
+  };
+
+  const handleCategory = (value: string) => {
+    setCategory(value);
+  };
 
   useEffect(() => {
     getALlCampaign()
@@ -41,29 +46,51 @@ const Campaigns = () => {
     campaignData = campaigns.filter(
       (campaign: any) => campaign.category === category
     );
-  } else {
+  }else {
     campaignData = campaigns;
   }
 
-  const handleCategory = (value: string) => {
-    setCategory(value);
-  };
+  if(searchQuery){
+    campaignData =  campaignData.filter((campaign:any)=> campaign.title.toLowerCase().includes(searchQuery.toLowerCase()))
+  }
+  
+  
 
   return (
     <div className="container">
       <div className="row justify-content-center">
-        <div className="col-3 my-5 category-bg">
-        </div>
+        <div className="col-3 my-5 category-bg"></div>
         <div className="col-9 pt-5 my-5 ps-5">
-        <div className="pt-2 pb-5 px-2"><Category handle={handleCategory}/></div>
-        {campaignData.length<=0 && <div className={classes.noCampaign}>No Campaign Found</div>}
-        {error && <div className={`text-center ${classes.error}`}> {error} </div>}
+
+          {/* Search Component */}
+          <div className="text-end">
+            <input type="text" 
+            placeholder="Search campaign for fundraisers" 
+            className={`mb-2 ${classes.custonSearch}`}
+            onChange={(e)=> setSearchQuery(e.target.value)}/>
+          </div>
+          <div className="pt-2 pb-5 px-2">
+            <Category handle={handleCategory} />
+          </div>
+          {campaignData.length <= 0 && (
+            <div className={classes.noCampaign}>No Campaign Found</div>
+          )}
+          {error && (
+            <div className={`text-center ${classes.error}`}> {error} </div>
+          )}
           <div className="row row-cols-1 row-cols-md-3 g-3">
             {campaignData.slice(0, visible).map((camapign: any) => (
-                <CampaignCard camapign={camapign}  key={camapign._id}/>
+              <CampaignCard camapign={camapign} key={camapign._id} />
             ))}
           </div>
-          {campaignData.length>=visible && <button className="btn btn-primary my-5" onClick={showMoreCampaigns}>Load More</button>}
+          {campaignData.length >= visible && (
+            <button
+              className="btn btn-primary my-5"
+              onClick={showMoreCampaigns}
+            >
+              Load More
+            </button>
+          )}
         </div>
       </div>
     </div>
